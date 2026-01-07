14ed2ea85cb208ec286ddf898be412f22fc847b1
Dev-Amna <amnaharaf272@gmail.com>
2026-01-07 12:34:40 +0500

    Replace Expo template with WhatsApp clone implementation

---
+++ src/app/_layout.tsx
@@ -0,0 +1,22 @@
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Redirect, Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+SplashScreen.preventAutoHideAsync();
+const RootNavigation = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hideAsync();
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+      <Stack />
+      {isLogin ? <Redirect href={"/(main)"} /> : <Redirect href={"/(auth)"} />}
+    </>
+  );
+};
+
+export default RootNavigation;
